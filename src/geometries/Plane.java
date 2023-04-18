8303d3ef9228a93e91784004c052da9d66b4f48f
אמיר חי תמר <amurhay@g.jct.ac.il>
2023-04-18 15:06:16 +0300

    updating

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -52,7 +52,8 @@ public class Plane implements Geometry {
 
     @Override
     public Vector getNormal(Point point) {
-        return null;
+       // return getNormal();
+        return normal;
     }
 
 }
