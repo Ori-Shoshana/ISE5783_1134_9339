9df228c3bc6d2e0262b0a22384a10862d09dc23a
Ori-Shoshana <orisho209@gmail.com>
2023-04-19 17:44:09 +0300

    update class test triangle and plane

--- unitTests/geometries/PlaneTests.java
+++ unitTests/geometries/PlaneTests.java
@@ -11,7 +11,20 @@ import primitives.Point;
 import primitives.Vector;
 
 class PlaneTests {
-
+    @Test
+    void testPlane() {
+        // ====Boundary Values Tests ====
+        // TC10: two points are the same
+        Point p1 = new Point( 1,  2, 3);
+        Point p2 = new Point( 1,  2,  3);
+        Point p3= new Point( 2,  3,  4);
+        assertThrows (IllegalArgumentException.class, ()->new Plane (p1, p2, p3), "ERROR: two points are the same");
+        //TC11: three points are on the same line
+        Point p4 = new Point(1,  1,  1);
+        Point p5 = new Point( 1,  1,  2);
+        Point p6 = new Point( 1,  1,  3);
+        assertThrows (IllegalArgumentException.class, () -> new Plane (p4, p5, p6), "ERROR: three points are on the same line");
+    }
     @Test
     public void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
--- unitTests/geometries/TriangleTests.java
+++ unitTests/geometries/TriangleTests.java
@@ -8,20 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleTests {
     @Test
-    void testPlane() {
-        // ====Boundary Values Tests ====
-        // TC10: two points are the same
-        Point p1 = new Point( 1,  2, 3);
-        Point p2 = new Point( 1,  2,  3);
-        Point p3= new Point( 2,  3,  4);
-        assertThrows (IllegalArgumentException.class, ()->new Plane (p1, p2, p3), "ERROR: two points are the same");
-        //TC11: three points are on the same line
-        Point p4 = new Point(1,  1,  1);
-        Point p5 = new Point( 1,  1,  2);
-        Point p6 = new Point( 1,  1,  3);
-        assertThrows (IllegalArgumentException.class, () -> new Plane (p4, p5, p6), "ERROR: three points are on the same line");
-    }
-    @Test
     void testGetNormal() {
     //=== Equivalence Partitions Tests ===
     //TC01: normal to triangle
