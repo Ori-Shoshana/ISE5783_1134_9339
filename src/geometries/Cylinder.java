d7cd2dce1342fceb4da24f229aac17c074027072
אמיר חי תמר <amurhay@g.jct.ac.il>
2023-04-19 13:17:11 +0300

    updating

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -4,6 +4,9 @@ import primitives.Point;
 import primitives.Vector;
 import primitives.Ray;
 
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
+
 
 /** Cylinder class which represents the location of a cylinder in space
  * @author Amir Hay and ori */
@@ -36,6 +39,25 @@ public class Cylinder extends Tube {
 
     @Override
     public Vector getNormal(Point point) {
-        return null;
+        Point p0 = axisRay.getP0();
+        Vector v = axisRay.getDir();
+
+        if (point.equals(p0))
+            return v;
+
+        // projection of P-p0 on the ray:
+        Vector u = point.subtract(p0);
+
+        // distance from p0 to the o who is in from of point
+        double t = alignZero(u.dotProduct(v));
+
+        // if the point is at a base
+        if (t == 0 || isZero(height - t))
+            return v;
+
+        //the other point on the axis facing the given point
+        Point o = p0.add(v.scale(t));
+
+        return point.subtract(o).normalize();
     }
 }
