757edc1bc29392368386fa6472b00bec265d6ced
Ori-Shoshana <orisho209@gmail.com>
2023-05-01 14:48:47 +0300

    final update stage 3

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -72,38 +72,56 @@ public class Sphere extends RadialGeometry {
      * @param ray The {@link Ray} object used to find the intersection.
      * @return A list of {@link Point} objects representing the intersection points, or null if no intersection is found.
      */
-    @Override
     public List<Point> findIntersections(Ray ray) {
 
+        // Get the starting point of the ray and its direction vector.
         Point p0 = ray.getP0();
         Vector v = ray.getDir();
+
+        // Initialize some variables.
         Vector u;
-        double tm;
-        double dsquare;
-        double rsquare = radius * radius;
+        double tm; // the parameter t where the ray intersects the sphere's center
+        double dsquare; // the distance squared between the ray and the sphere's center
+        double rsquare = radius * radius; // the radius of the sphere squared
+
+        // Check if the ray starts from the center of the sphere.
         if (center.equals(p0)) {
-            tm = 0;
-            dsquare = -tm * tm;
-        }
-        else {
+            tm = 0; // the ray intersects the center at t=0
+            dsquare = -tm * tm; // the distance squared between the ray and the center is 0
+        } else {
+            // Calculate the vector between the center of the sphere and the starting point of the ray.
             u = center.subtract(p0);
+            // Calculate the parameter t where the ray intersects the sphere's center.
             tm = v.dotProduct(u);
+            // Calculate the distance squared between the ray and the sphere's center.
             dsquare = u.dotProduct(u) - tm * tm;
         }
+
+        // Check if the ray misses the sphere.
         if (dsquare >= rsquare) {
             return null;
         }
+
+        // Calculate the distance from the intersection point(s) to the starting point of the ray.
         double th = Math.sqrt(rsquare - dsquare);
+
+        // Check if the ray intersects the sphere at two points.
         if (alignZero(tm + th) > 0) {
             if (alignZero(tm - th) > 0) {
+                // Return a list of two intersection points.
                 return List.of(ray.getPoint(tm + th), ray.getPoint(tm - th));
             }
+            // Return a list of one intersection point.
             return List.of(ray.getPoint(tm + th));
         }
+
+        // Check if the ray intersects the sphere at one point.
         if (alignZero(tm - th) > 0) {
+            // Return a list of one intersection point.
             return List.of(ray.getPoint(tm - th));
         }
+
+        // If the ray doesn't intersect the sphere, return null.
         return null;
-        //return List.of((tm+th!=0?p0.add(v.scale(tm+th)):null),p0.add(v.scale(tm-th)));
     }
 }
