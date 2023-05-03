c511eecafeb18df084d2dfe7001bbaa973c8d14a
Ori-Shoshana <orisho209@gmail.com>
2023-05-03 20:00:59 +0300

    new func in ray

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,5 +1,6 @@
 package primitives;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -9,18 +10,19 @@ import java.util.Objects;
  */
 public class Ray {
     /**
-     The starting point of the ray.
+     * The starting point of the ray.
      */
     final private Point p0;
     /**
-     The direction of the ray, normalized to a unit vector.
+     * The direction of the ray, normalized to a unit vector.
      */
     final private Vector dir;
 
     /**
-     Constructs a new Ray with the given starting point and direction.
-     @param p0 the starting point of the ray.
-     @param dir the direction of the ray.
+     * Constructs a new Ray with the given starting point and direction.
+     *
+     * @param p0  the starting point of the ray.
+     * @param dir the direction of the ray.
      */
     public Ray(Point p0, Vector dir) {
         this.p0 = p0;
@@ -28,30 +30,57 @@ public class Ray {
     }
 
     /**
-     Returns the starting point of the ray.
-     @return the starting point of the ray.
+     * Returns the starting point of the ray.
+     *
+     * @return the starting point of the ray.
      */
     public Point getP0() {
         return p0;
     }
 
     /**
-     Returns the direction of the ray.
-     @return the direction of the ray.
+     * Returns the direction of the ray.
+     *
+     * @return the direction of the ray.
      */
     public Vector getDir() {
         return dir;
     }
 
     /**
-     Returns the point at the given parameter value t along the ray.
-     @param t the parameter value.
-     @return the point at the given parameter value t along the ray.
+     * Returns the point at the given parameter value t along the ray.
+     *
+     * @param t the parameter value.
+     * @return the point at the given parameter value t along the ray.
      */
-    public Point getPoint(double t){
+    public Point getPoint(double t) {
         return p0.add(dir.scale(t));
     }
 
+    /**
+     * Finds the point in the given list that is closest to the starting point of this ray.
+     *
+     * @param list the list of points to search
+     * @return the closest point, or {@code null} if the list is {@code null} or empty
+     */
+    public Point findClosestPoint(List<Point> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+
+        Point closestPoint = list.get(0);
+        double closestDistance = closestPoint.distanceSquared(this.p0);
+
+        for (Point currentPoint : list) {
+            double currentDistance = currentPoint.distanceSquared(this.p0);
+            if (currentDistance < closestDistance) {
+                closestPoint = currentPoint;
+                closestDistance = currentDistance;
+            }
+        }
+
+        return closestPoint;
+    }
 
     @Override
     public boolean equals(Object o) {
