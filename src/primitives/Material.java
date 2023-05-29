c8bd750d0b948bdcac18cb130a32187dd67e7a6c
Ori-Shoshana <orisho209@gmail.com>
2023-05-29 19:18:06 +0300

    stage 7 update

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -16,6 +16,19 @@ public class Material {
     public Double3 kS = Double3.ZERO;
 
     /**
+     *  Kt - transparency component
+     * 0.0 is opaque
+     * 1.0 is clear
+     */
+    public Double3 kT = Double3.ZERO;
+
+    /**
+     *  Kr - reflection component
+     * 0.0 is matte
+     * 1.0 is very reflexive
+     */
+    public Double3 kR = Double3.ZERO;
+    /**
      * The material shininess
      */
     public int nShininess = 0;
@@ -73,6 +86,46 @@ public class Material {
      */
     public Material setnShininess(int nShininess) {
         this.nShininess = nShininess;
+        return this;
+    }
+    /**
+     * Sets the transparency coefficient of the material.
+     *
+     * @param kT The transparency coefficient to set.
+     * @return This Material object with the updated transparency coefficient.
+     */
+    public Material setkT(Double3 kT) {
+        this.kT = kT;
+        return this;
+    }
+    /**
+     * Sets the reflection coefficient of the material.
+     *
+     * @param kR The reflection coefficient to set.
+     * @return This Material object with the updated reflection coefficient.
+     */
+    public Material setkR(Double3 kR) {
+        this.kR = kR;
+        return this;
+    }
+    /**
+     * Sets the transparency coefficient of the material.
+     *
+     * @param kT The transparency coefficient to set.
+     * @return This Material object with the updated transparency coefficient.
+     */
+    public Material setkT(double kT) {
+        this.kT=new Double3(kT);
+        return this;
+    }
+    /**
+     * Sets the reflection coefficient of the material.
+     *
+     * @param kR The reflection coefficient to set.
+     * @return This Material object with the updated reflection coefficient.
+     */
+    public Material setkR(double kR) {
+        this.kR=new Double3(kR);
         return this;
     }
 }
